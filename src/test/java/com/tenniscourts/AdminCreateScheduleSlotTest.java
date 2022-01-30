76c20dde8d1dceea5832c6a1739075dd927b720e
Zafiu Andrei <zafiuandrei@yahoo.com>
2022-01-30 23:48:16 +0200

    We are now able to create a court schedule slot for the same time slot if it is for different courts

--- src/test/java/com/tenniscourts/AdminCreateScheduleSlotTest.java
+++ src/test/java/com/tenniscourts/AdminCreateScheduleSlotTest.java
@@ -1,6 +1,5 @@
 package com.tenniscourts;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +8,16 @@ import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AdminCreateScheduleSlotTest {
 
-  public static final LocalDateTime NOW = LocalDateTime.now();
-  public static final TimeSlot ONE_HOUR_AGO_TIME_SLOT = TimeSlot.of(NOW.minus(1, ChronoUnit.HOURS));
+  private static final LocalDateTime NOW = LocalDateTime.now();
+  private static final TimeSlot CURRENT_TIME_SLOT = TimeSlot.of(NOW);
+  private static final TimeSlot ONE_HOUR_AGO_TIME_SLOT =
+      TimeSlot.of(NOW.minus(1, ChronoUnit.HOURS));
   private final Court arthurAshe = new Court(1L, "Arthur Ashe");
+  private final Court rodLaver = new Court(2L, "Rod Laver");
 
   @Test
   @DisplayName(
@@ -35,8 +38,7 @@ class AdminCreateScheduleSlotTest {
       "Returns two court schedule slots for the same court, after adding two different time slots for the same court, given an initial empty schedule")
   void test1() {
     final var courtScheduler = new CourtScheduler();
-    final var currentTimeSlot = TimeSlot.of(NOW);
-    courtScheduler.createScheduleSlot(arthurAshe, currentTimeSlot);
+    courtScheduler.createScheduleSlot(arthurAshe, CURRENT_TIME_SLOT);
 
     courtScheduler.createScheduleSlot(arthurAshe, ONE_HOUR_AGO_TIME_SLOT);
 
@@ -44,7 +46,7 @@ class AdminCreateScheduleSlotTest {
 
     final var courtScheduleSlot = courtScheduler.getCourtScheduleSlots().get(0);
     assertThat(courtScheduleSlot.getCourt()).isEqualTo(arthurAshe);
-    assertThat(courtScheduleSlot.getTimeSlot()).isEqualTo(currentTimeSlot);
+    assertThat(courtScheduleSlot.getTimeSlot()).isEqualTo(CURRENT_TIME_SLOT);
 
     final var courtSecondScheduleSlot = courtScheduler.getCourtScheduleSlots().get(1);
     assertThat(courtSecondScheduleSlot.getCourt()).isEqualTo(arthurAshe);
@@ -60,5 +62,28 @@ class AdminCreateScheduleSlotTest {
 
     assertThatThrownBy(() -> courtScheduler.createScheduleSlot(arthurAshe, TimeSlot.of(NOW)))
         .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName(
+      "Returns two court schedule slots for two different courts, after adding same time slots for two different courts, given an initial empty schedule")
+  void test3() {
+    final var courtScheduler = new CourtScheduler();
+    courtScheduler.createScheduleSlot(arthurAshe, CURRENT_TIME_SLOT);
+    courtScheduler.createScheduleSlot(rodLaver, CURRENT_TIME_SLOT);
+
+    assertThat(courtScheduler.getCourtScheduleSlots()).hasSize(2);
+
+    assertSchedulerContainsInterestedCourtScheduleSlot(
+        courtScheduler, new CourtScheduleSlot(arthurAshe, CURRENT_TIME_SLOT));
+    assertSchedulerContainsInterestedCourtScheduleSlot(
+        courtScheduler, new CourtScheduleSlot(rodLaver, CURRENT_TIME_SLOT));
+  }
+
+  private void assertSchedulerContainsInterestedCourtScheduleSlot(
+      CourtScheduler courtScheduler, CourtScheduleSlot interestedCourtScheduleSlot) {
+    assertTrue(
+        courtScheduler.getCourtScheduleSlots().stream()
+            .anyMatch(courtScheduleSlot -> courtScheduleSlot.equals(interestedCourtScheduleSlot)));
   }
 }
