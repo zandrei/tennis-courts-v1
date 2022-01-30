4ffdec819865f13d685b924b28648b3657a7042d
Zafiu Andrei <zafiuandrei@yahoo.com>
2022-01-31 00:05:23 +0200

    Small cleanup

--- src/test/java/com/tenniscourts/PlayerReadCourtSchedulesTest.java
+++ src/test/java/com/tenniscourts/PlayerReadCourtSchedulesTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PlayerReadCourtSchedulesTest {
+class PlayerReadCourtSchedulesTest {
 
   private CourtScheduler courtScheduler;
 
