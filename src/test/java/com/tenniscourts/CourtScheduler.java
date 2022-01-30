2b8a3ed006d7c7d4ef5139d43ee5718cbd851423
Zafiu Andrei <zafiuandrei@yahoo.com>
2022-01-30 23:52:59 +0200

    Moved getFreeScheduleSlots method to ReservationSystem

--- src/test/java/com/tenniscourts/CourtScheduler.java
+++ src/test/java/com/tenniscourts/CourtScheduler.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
-import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 
 public class CourtScheduler {
@@ -13,10 +12,6 @@ public class CourtScheduler {
   private final HashMap<Court, List<CourtScheduleSlot>> courtScheduleSlots = new HashMap<>();
 
   CourtScheduler() {}
-
-  public List<CourtScheduleSlot> getFreeScheduleSlots() {
-    return emptyList();
-  }
 
   public void createScheduleSlot(Court court, TimeSlot timeSlot) {
     if (timeSlotIsInvalidForCourt(court, timeSlot)) {
--- src/test/java/com/tenniscourts/PlayerReadCourtSchedulesTest.java
+++ src/test/java/com/tenniscourts/PlayerReadCourtSchedulesTest.java
@@ -2,14 +2,23 @@ package com.tenniscourts;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerReadCourtSchedulesTest {
 
   @Test
   public void returnsEmptyScheduleSlots_GivenASchedulerThatHasNoEmptySlots() {
-    final var courtScheduler = new CourtScheduler();
+    final var reservationSystem = new ReservationSystem();
+    assertThat(reservationSystem.getFreeScheduleSlots()).isEmpty();
+  }
 
-    assertThat(courtScheduler.getFreeScheduleSlots()).isEmpty();
+  public static class ReservationSystem {
+
+    public List<CourtScheduleSlot> getFreeScheduleSlots() {
+      return emptyList();
+    }
   }
 }
